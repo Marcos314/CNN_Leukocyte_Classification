7552c895e031fb294b3c6a6199e5c3680a098ff5
marcos314 <marcoswesley314@gmail.com>
2020-04-01 17:55:21 -0300

    tratamento

--- scripts/replaceLine.py
+++ scripts/replaceLine.py
@@ -1,10 +1,10 @@
 import sh
 import os
 
-novaLinha = '"MES REFERENCIA";"MES COMPETENCIA";"UF";"CODIGO MUNICIPIO SIAFI";"NOME MUNICIPIO";"NIS FAVORECIDO";"NOME FAVORECIDO";"VALOR PARCELA"'
-directory = os.fsencode("/home/mendes/Downloads")
+novaLinha = 'MES_REFERENCIA;MES_COMPETENCIA;UF;CODIGO_MUNICIPIO_SIAFI;NOME_MUNICIPIO;NIS_FAVORECIDO;NOME_FAVORECIDO;VALOR_PARCELA'
+directory = os.fsencode("/var/lib/neo4j/import/")
 
 for file in os.listdir(directory):
      filename = os.fsdecode(file)
-     if filename.endswith(".csv"): 
+     if filename.endswith("1.csv"): 
          sh.sed("-i", "1s/.*/" + novaLinha + "/", filename)
